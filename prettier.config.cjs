35834938ade30dfba4f5c8dd22f45d1aeb81ab25
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-18 22:17:57 +0530

    add: prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+import { module } from "module";
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+  printWidth: 110,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
