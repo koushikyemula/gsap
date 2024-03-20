badef5025970d1313b889b00d5110cb1bcc7bb26
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-20 16:37:56 +0530

    chore: hero video and prettier changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,42 @@
-import gsap from "gsap"
-import { useGSAP} from "@gsap/react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth > 768 ? heroVideo : smallHeroVideo);
   useGSAP(() => {
-    gsap.to("#hero", {opacity: 1, delay: 1.5})
-  }, [])
-  
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
+
+  const handleSetVideo = () => {
+    if (window.innerWidth > 768) {
+      setVideoSrc(heroVideo);
+    } else {
+      setVideoSrc(smallHeroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleSetVideo);
+    return () => window.removeEventListener("resize", handleSetVideo);
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 flex w-full flex-center flex-col">
-        <p id="hero" className="hero-title">Iphone 16 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 16 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video autoPlay muted playsInline={true} key={videoSrc} className="pointer-events-none">
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
-  )
+  );
 };
 
 export default Hero;
