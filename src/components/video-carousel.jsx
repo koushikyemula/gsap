b068cfcf2dc7b4077fa1ee18235db56c54747fc9
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-21 15:20:51 +0530

    chore: video refs, usestates, useeffects, and pills

--- src/components/video-carousel.jsx
+++ src/components/video-carousel.jsx
@@ -1,6 +1,34 @@
+import { useRef } from "react";
 import { hightlightsSlides } from "../constants";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [loadedData, setLoadedData] = useState([]);
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
+
   return (
     <>
       <div className="flex items-center">
@@ -8,7 +36,15 @@ const VideoCarousel = () => {
           <div key={slide.id} className="sm:pr-20 pr-10" id="slider">
             <div className="video-carousel-container">
               <div className="w-full h-full overflow-hidden rounded-3xl bg-black flex-center">
-                <video preload="auto" id="video" playsInline={true} muted>
+                <video
+                  preload="auto"
+                  id="video"
+                  playsInline={true}
+                  muted
+                  ref={(e) => (videoRef.current[idx] = e)}
+                  onPlay={() => {
+                    setVideo((prev) => ({ ...prev, isPlaying: true }));
+                  }}>
                   <source src={slide.video} type="video/mp4" />
                 </video>
               </div>
@@ -22,6 +58,21 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {videoRef.current.map((_, idx) => (
+            <span
+              ref={(e) => (videoDivRef.current[idx] = e)}
+              key={idx}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer">
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(e) => (videoSpanRef.current[idx] = e)}
+              />
+            </span>
+          ))}
+        </div>
       </div>
     </>
   );
