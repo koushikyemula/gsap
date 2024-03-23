c16a7063f6856d5d31d8107035dd3f81814d28b3
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-03-23 20:12:10 +0530

    chore: styling changes

--- src/components/highlights.jsx
+++ src/components/highlights.jsx
@@ -9,9 +9,9 @@ const Highlights = () => {
     gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
   return (
-    <section id="highlights" className="w-screen h-full common-padding overflow-hidden bg-zinc">
+    <section id="highlights" className="w-screen h-full sm:!pb-5 common-padding overflow-hidden bg-zinc">
       <div className="screen-max-width">
-        <div className="mb-12 w-full md:flex items-end justify-between">
+        <div className="mb-10 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">
             Get the highlights.
           </h1>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <img src={appleImg} alt="Apple" className="cursor-pointer" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((item, idx) => (
             <div key={idx} className="px-5 text-sm text-gray hover:text-white cursor-pointer transition-all">
@@ -14,8 +14,8 @@ const NavBar = () => {
           ))}
         </div>
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src={searchImg} alt="search" height={18} width={18} />
-          <img src={bagImg} alt="Bag" height={18} width={18} />
+          <img src={searchImg} alt="search" className="cursor-pointer" height={18} width={18} />
+          <img src={bagImg} alt="Bag" className="cursor-pointer" height={18} width={18} />
         </div>
       </nav>
     </header>
